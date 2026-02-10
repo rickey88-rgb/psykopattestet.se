678bd8a633808d5dc4de973175e23ef0c42e3614
rickey88-rgb <rickey88@hotmail.se>
2026-02-10 19:12:04 +0100

    Delete psykopattestet-se-repo-maxxed directory

--- psykopattestet-se-repo-maxxed/app/robots.ts
+++
@@ -1,5 +0,0 @@
-import { MetadataRoute } from "next";
-import { SITE } from "@/lib/site";
-export default function robots(): MetadataRoute.Robots {
-  return { rules:[{ userAgent:"*", allow:"/" }], sitemap: `${SITE.url}/sitemap.xml` };
-}
--- psykopattestet-se-repo-maxxed/app/sitemap.ts
+++
@@ -1,7 +0,0 @@
-import { MetadataRoute } from "next";
-import { SITE } from "@/lib/site";
-const pages = ["", "/psykopatiska-drag-i-relationer", "/psykopat-eller-narcissist", "/tecken-pa-manipulation-i-relation", "/nar-magkanslan-inte-slapper", "/snabb-check-psykopatiska-monster", "/gaslighting", "/skuldbelagging", "/kontrollbeteende", "/tystnad-som-straff", "/love-bombing", "/traumabindning", "/idealiseringsfasen-och-nedvardering", "/granssattning-reaktioner", "/isolering-i-relation", "/hot-och-ultimatum"];
-export default function sitemap(): MetadataRoute.Sitemap {
-  const now = new Date();
-  return pages.map((p)=>({ url:`${SITE.url}${p}`, lastModified: now, changeFrequency:"monthly", priority: p===""?1:0.75 }));
-}
